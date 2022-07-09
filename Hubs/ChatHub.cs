7ea100253277983bd7f8d0272fc31347f317f668
Sallal Haider <87648696+Sidhugee@users.noreply.github.com>
2022-07-09 10:59:24 +0100

    Add files via upload

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorChatSignalR.Server.Hubs
+{
+    public class ChatHub: Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await AddMessageToChat("", "user connected");
+            await base.OnConnectedAsync();
+        }
+
+
+
+        public async Task AddMessageToChat(string user, string message)
+        {
+            await Clients.All.SendAsync("GetThatMessageDude" ,user, message);
+        }
+    }
+}
